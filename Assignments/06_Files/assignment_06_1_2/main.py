e580c9fa40fba195768fffa6542c7e030a8230fa
David Docampo <david@opsone.ch>
2025-11-04 11:28:21 +0100

    remove unused code

--- Assignments/06_Files/assignment_06_1_2/main.py
+++ Assignments/06_Files/assignment_06_1_2/main.py
@@ -1,19 +1,6 @@
 from ui import menu
 from database import Database
 
-
-def main_menu():
-    while True:
-        print('=======================================')
-        print('‖         Student Database 👨🏼‍🎓         ‖')
-        print('=======================================')
-        print()
-        print()
-        print('1. Add new student')
-        print('2. Search for student')
-        print('3. Exit')
-
-
 file_name = 'assignment_06_1_2.csv'
 
 db = Database(file_name)
