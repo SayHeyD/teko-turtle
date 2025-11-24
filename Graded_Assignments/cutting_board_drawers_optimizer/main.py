13e522cf65ed10835b0a34e7db0e129b475e81fd
David Docampo <david@opsone.ch>
2025-11-24 15:15:07 +0100

    feat: WIP: program entrypoint

---
+++ Graded_Assignments/cutting_board_drawers_optimizer/main.py
@@ -0,0 +1,14 @@
+"""
+Programm for optimizing cutting board layouts across multiple kitchen drawers
+"""
+
+def main() -> None:
+    """
+    Main entrypoint for the program.
+    :return:
+    """
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
