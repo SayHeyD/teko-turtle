3b6459035c97773265d1f2c6ec4e7dc62cea97b5
David Docampo <david@opsone.ch>
2025-11-24 15:45:46 +0100

    chore: add basic tests for drawer and cutting_board class

---
+++ Graded_Assignments/cutting_board_drawers_optimizer/tests/test_drawer.py
@@ -0,0 +1,16 @@
+import pytest
+
+from drawer import Drawer
+
+@pytest.fixture
+def get_drawer():
+    return Drawer(2000, 1500, 5300)
+
+def test_that_drawer_returns_correct_length(get_drawer):
+    assert get_drawer.get_length() == 2000
+
+def test_that_drawer_returns_correct_width(get_drawer):
+    assert get_drawer.get_width() == 1500
+
+def test_that_drawer_returns_correct_max_load(get_drawer):
+    assert get_drawer.get_max_load() == 5300
