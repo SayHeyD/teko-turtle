53a81932d093a103f67a855f464515667e7b0663
David Docampo <david@opsone.ch>
2025-11-26 18:22:22 +0100

    chore(tests): add error message matching

--- Graded_Assignments/cutting-board-drawers-optimizer/tests/test_cutting_board.py
+++ Graded_Assignments/cutting-board-drawers-optimizer/tests/test_cutting_board.py
@@ -8,19 +8,19 @@ def get_cutting_board():
     return CuttingBoard(65, 24, 2560, 2590)
 
 def test_that_cutting_board_constructor_raises_value_error_if_length_is_less_than_1(get_cutting_board):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match=r'.*length.*'):
         CuttingBoard(0, 24, 2560, 2590)
 
 def test_that_cutting_board_constructor_raises_value_error_if_width_is_less_than_1(get_cutting_board):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match=r'.*width.*'):
         CuttingBoard(65, 0, 2560, 2590)
 
 def test_that_cutting_board_constructor_raises_value_error_if_weight_is_less_than_1(get_cutting_board):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match=r'.*weight.*'):
         CuttingBoard(65, 24, 0, 2590)
 
 def test_that_cutting_board_constructor_raises_value_error_if_price_is_less_than_1(get_cutting_board):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match=r'.*price.*'):
         CuttingBoard(65, 24, 2560, 0)
 
 def test_that_get_price_in_chf_returns_correct_float(get_cutting_board):
