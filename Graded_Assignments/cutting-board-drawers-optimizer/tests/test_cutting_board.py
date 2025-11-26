2c77a3fd9fe365521117fccd9ba26100d0ec8b10
David Docampo <david@opsone.ch>
2025-11-26 18:28:28 +0100

    chore(tests): add tests for new class methods

--- Graded_Assignments/cutting-board-drawers-optimizer/tests/test_cutting_board.py
+++ Graded_Assignments/cutting-board-drawers-optimizer/tests/test_cutting_board.py
@@ -7,19 +7,19 @@ from cutting_board_drawers_optimizer.optimizer import CuttingBoard
 def get_cutting_board():
     return CuttingBoard(65, 24, 2560, 2590)
 
-def test_that_cutting_board_constructor_raises_value_error_if_length_is_less_than_1(get_cutting_board):
+def test_that_cutting_board_constructor_raises_value_error_if_length_is_less_than_1():
     with pytest.raises(ValueError, match=r'.*length.*'):
         CuttingBoard(0, 24, 2560, 2590)
 
-def test_that_cutting_board_constructor_raises_value_error_if_width_is_less_than_1(get_cutting_board):
+def test_that_cutting_board_constructor_raises_value_error_if_width_is_less_than_1():
     with pytest.raises(ValueError, match=r'.*width.*'):
         CuttingBoard(65, 0, 2560, 2590)
 
-def test_that_cutting_board_constructor_raises_value_error_if_weight_is_less_than_1(get_cutting_board):
+def test_that_cutting_board_constructor_raises_value_error_if_weight_is_less_than_1():
     with pytest.raises(ValueError, match=r'.*weight.*'):
         CuttingBoard(65, 24, 0, 2590)
 
-def test_that_cutting_board_constructor_raises_value_error_if_price_is_less_than_1(get_cutting_board):
+def test_that_cutting_board_constructor_raises_value_error_if_price_is_less_than_1():
     with pytest.raises(ValueError, match=r'.*price.*'):
         CuttingBoard(65, 24, 2560, 0)
 
@@ -30,3 +30,12 @@ def test_that_get_price_in_chf_returns_correct_float_length(get_cutting_board):
     price = str(get_cutting_board.get_price_in_chf())
     decimal_chars = len(price.split('.')[1])
     assert decimal_chars == 2
+
+def test_that_get_length_in_centimeters_returns_correct_float(get_cutting_board):
+    assert get_cutting_board.get_length_in_centimeters() == 65
+
+def test_that_get_width_in_centimeters_returns_correct_float(get_cutting_board):
+    assert get_cutting_board.get_width_in_centimeters() == 24
+
+def test_that_get_weight_in_grams_returns_correct_float(get_cutting_board):
+    assert get_cutting_board.get_weight_in_grams() == 2560
