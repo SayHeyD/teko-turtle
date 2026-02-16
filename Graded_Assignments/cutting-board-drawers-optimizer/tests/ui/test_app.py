75ff6c1da1c8aad72c55314160f7d16b39dbb200
David Docampo <david@opsone.ch>
2026-02-16 09:08:17 +0100

    chore(tests): remove unused imports

--- Graded_Assignments/cutting-board-drawers-optimizer/tests/ui/test_app.py
+++ Graded_Assignments/cutting-board-drawers-optimizer/tests/ui/test_app.py
@@ -1,11 +1,9 @@
 import os
 import pytest
 from textual.widgets import Input, TabbedContent
-from cutting_board_drawers_optimizer.ui import CuttingBoardDrawersOptimizerApp
 from cutting_board_drawers_optimizer.ui.save_dialog import SaveDialog
 from cutting_board_drawers_optimizer.ui.load_dialog import LoadDialog
 from cutting_board_drawers_optimizer.ui.cutting_board_manager import CuttingBoardManager
-from cutting_board_drawers_optimizer.ui.drawer_manager import DrawerManager
 from cutting_board_drawers_optimizer.ui.cutting_board_table import CuttingBoardTable
 
 from textual.widgets import Header, Footer
